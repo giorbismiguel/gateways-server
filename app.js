80802e2bd6278bc6d2647e3d401d2d9e5ab038fa
Giorbis Miguel <giorbismiguel@gmail.com>
2020-08-19 13:59:21 -0400

    Add handlers for cors errors

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
+        return res.status(200).json({}); 
+    }
+    next();
+});
 app.use(morgan("dev"));
 
 const gatewaysRoutes = require("./api/routes/gateways");
