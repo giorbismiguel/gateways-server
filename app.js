7fe52d71646ddf8a33ebe535ed4860b04961e9ef
Giorbis Miguel <giorbismiguel@gmail.com>
2020-08-19 13:41:38 -0400

    handling general errors and log request for better resolve of problem

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 const gatewaysRoutes = require("./api/routes/gateways");
-
 app.use("/gateways", gatewaysRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 app.listen(3000);
