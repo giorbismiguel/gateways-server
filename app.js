ff468cd783fb01bc62df29f8e9a37187b8d44936
Giorbis Miguel <giorbismiguel@gmail.com>
2020-08-19 23:35:07 -0400

    Add mongoose and cros

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
-        return res.status(200).json({}); 
-    }
-    next();
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE");
+    return res.status(200).json({});
+  }
+  next();
 });
 app.use(morgan("dev"));
 
 const gatewaysRoutes = require("./api/routes/gateways");
 app.use("/gateways", gatewaysRoutes);
+
+mongoose.connect(
+  "mongodb+srv://giorbismiguel:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0-drksp.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 app.use((req, res, next) => {
   const err = new Error("Not Found");
